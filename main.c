c6b78e41542246bc871bc4645a5441143c2568eb
johnstonew <johnstonewahome@gmail.com>
2023-03-22 07:17:03 -0700

    print outputs

--- main.c
+++ main.c
@@ -1,9 +1,22 @@
 #include "monty.h"
 
 int main(int argc, char* argv[]){
-    int i;
-    
-    for(i = 0; i < argc; i++){
-        printf("%s\n", argv[i]);
-     }
+	if (argc < 2 || argc > 2){
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	} else {
+		FILE *fp = fopen(argv[1],"r");
+		char line[150];
+
+    		if (fp == NULL){
+       			fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+       			exit(EXIT_FAILURE);
+    		} else {
+			while(!feof(fp)) {
+				fgets(line, 150, fp);
+				printf("%s", line);
+			}
+		}
+    		fclose(fp);
+} 
 }
