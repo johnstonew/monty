97388110aef4bfa3822f729b3a7790253d3e43d9
johnstonew <johnstonewahome@gmail.com>
2023-03-23 15:30:59 -0700

    add pop task

--- monty.h
+++ monty.h
@@ -65,6 +65,10 @@ void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
 void free_stack(stack_t *head);
 void f_pint(stack_t **head, unsigned int number);
+void f_pop(stack_t **head, unsigned int counter);
+void f_swap(stack_t **head, unsigned int counter);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
 
 
 #endif
