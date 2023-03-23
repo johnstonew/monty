b06a9982c54d7f10e130a175540b128765ae7d64
johnstonew <johnstonewahome@gmail.com>
2023-03-23 15:49:56 -0700

    updates

--- monty.h
+++ monty.h
@@ -66,9 +66,6 @@ void addqueue(stack_t **head, int n);
 void free_stack(stack_t *head);
 void f_pint(stack_t **head, unsigned int number);
 void f_pop(stack_t **head, unsigned int counter);
-void f_swap(stack_t **head, unsigned int counter);
-void f_add(stack_t **head, unsigned int counter);
-void f_nop(stack_t **head, unsigned int counter);
 
 
 #endif
